e077341e989085db42133e02f0ef5924b636727e
Fredferik Kjaer <f.kjaer@icloud.com>
2021-03-04 15:50:06 +0100

    Udkast til vores scripts

---
+++ MainScript.py
@@ -0,0 +1,10 @@
+from network import *
+from TrainScript import *
+model = RecurrentAutoencoder(seq_len, n_features, 128)
+model = model.to(device)
+model, history = train_model(
+    model,
+    train_dataset,
+    val_dataset,
+    n_epochs=150
+)
