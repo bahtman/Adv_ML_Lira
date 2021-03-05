c8fb62b2996c9eed32c7ee90009b68e91298f669
Fredferik Kjaer <f.kjaer@icloud.com>
2021-03-05 09:52:50 +0100

    Prøver at sætte main script op

--- MainScript.py
+++ MainScript.py
@@ -2,6 +2,8 @@ from Network import *
 from TrainScript import *
 from Sim_Data import *
 print(labels)
+seq_len = len(timeseries)
+n_features = len(timeseries[1])#Skal vist være 10?
 model = RecurrentAutoencoder(seq_len, n_features, 128)
 #model = model.to(device)
 model, history = train_model(
