5520e6575e65fbbc7389f318f806df7a4d705995
Fredferik Kjaer <f.kjaer@icloud.com>
2021-03-05 10:39:09 +0100

    Load Data

--- MainScript.py
+++ MainScript.py
@@ -1,19 +1,17 @@
 from Network import *
 from TrainScript import *
-from Sim_Data import *
+from src.dataset import *
 from torch.utils.data import random_split
-
-print(labels)
-seq_len = len(timeseries)
-n_features = len(timeseries[1])#Skal vist være 10?
+data = TS_dataset()
+seq_len = len(data)
+n_features = len(data[0][0])#Skal vist være 10?
 model = RecurrentAutoencoder(seq_len, n_features, 128)
 #model = model.to(device)
 
 val_percent = 0.1
-dataset = TimeSeriesConvertedToTensor #Det skal vel convertes til tensor
-n_val = int(len(dataset) * val_percent)
-n_train = len(dataset) - n_val
-train, val = random_split(dataset, [n_train, n_val])
+n_val = int(len(data) * val_percent)
+n_train = len(data) - n_val
+train, val = random_split(data, [n_train, n_val])
 
 model, history = train_model(
     model,
