9c3506c969cd8d8856a26c4f99836e14c6c4f682
Carl Johan Astrup <33057596+Fiskehandleren@users.noreply.github.com>
2021-03-11 16:07:15 +0100

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -49,35 +49,34 @@ class TS_dataset(Dataset):
         self.process_data()
 
     def process_data(self):
-        #Normalization
+        # Normalization
         standscaler = StandardScaler()
-        mscaler = MinMaxScaler(feature_range=(0,1))
+        mscaler = MinMaxScaler(feature_range=(0, 1))
         self.data[self.columns] = standscaler.fit_transform(self.data[self.columns])
         self.data[self.columns] = mscaler.fit_transform(self.data[self.columns])
-        
-        #Init output arrays
+
+        # Init output arrays
         self.all_data = np.array([])
         self.labels = np.array([])
 
-        #Extract data from dataframe
-        d_array = self.data[self.columns].values  
+        # Extract data from dataframe
+        d_array = self.data[self.columns].values
         label_array = self.data['labels'].values
 
-        #Generate snippets with length equal to "timesteps"
-        for index in range(self.data.shape[0]-self.timesteps+1):
-            this_array = d_array[index:index+self.timesteps].reshape((-1,self.timesteps,len(self.columns)))
-            timesteps_label = label_array[index:index+self.timesteps]
-            if np.any(timesteps_label==-1): #If any single observation in snippet is defect, the snippet is defect.
+        # Generate snippets with length equal to "timesteps"
+        for index in range(self.data.shape[0] - self.timesteps + 1):
+            this_array = d_array[index:index + self.timesteps].reshape((-1, self.timesteps, len(self.columns)))
+            timesteps_label = label_array[index:index + self.timesteps]
+            if np.any(timesteps_label == -1):  # If any single observation in snippet is defect, the snippet is defect.
                 this_label = -1
             else:
                 this_label = 1
             if self.all_data.shape[0] == 0:
                 self.all_data = this_array
-                self.labels = this_label                    
+                self.labels = this_label
             else:
-                self.all_data = np.concatenate([self.all_data,this_array],axis=0)
-                self.labels = np.append(self.labels,this_label)
-
+                self.all_data = np.concatenate([self.all_data, this_array], axis=0)
+                self.labels = np.append(self.labels, this_label)
 
     def __len__(self):
         return len(self.labels)
@@ -85,7 +84,7 @@ class TS_dataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        data = self.all_data[idx,:,:]
+        data = self.all_data[idx, :, :]
         label = self.labels[idx]
-        
+
         return data, label
