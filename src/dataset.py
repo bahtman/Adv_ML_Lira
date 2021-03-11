ed0be72ecd3a87fb371eca14ba538a411364a155
Carl Johan Astrup <33057596+Fiskehandleren@users.noreply.github.com>
2021-03-11 16:07:03 +0100

    Signals -> samples. Also spacing between operators </3

--- src/dataset.py
+++ src/dataset.py
@@ -4,7 +4,7 @@ import symengine
 import timesynth as ts
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler,MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
 class TS_dataset(Dataset):
@@ -12,42 +12,41 @@ class TS_dataset(Dataset):
     def __init__(self, datafile=None, timesteps=10):
         self.timesteps = timesteps
         if datafile:
-            True
-            #dataset = pickle.load(open(datafile,'rb'))
-            #data = pd.DataFrame.from_dict(dataset)
-            #self.columns = blabla
+            dataset = pickle.load(open(datafile, 'rb'))
+            data = pd.DataFrame.from_dict(dataset)
         else:
-            ###Initialize samplers
+            # Initialize samplers
             time_sampler = ts.TimeSampler(stop_time=20)
-            
-            #Regular signal
+
+            # Regular signal
             regular_time_samples = time_sampler.sample_regular_time(num_points=1000)
             reg_signalgen = ts.signals.Sinusoidal(frequency=0.25)
             noise = ts.noise.GaussianNoise(std=0.1)
             reg_timeseries = ts.TimeSeries(reg_signalgen, noise_generator=noise)
-            #Defect signal
+
+            # Defect signal
             irregular_time_samples = time_sampler.sample_irregular_time(num_points=2000, keep_percentage=50)
             irreg_signalgen = ts.signals.ar.AutoRegressive(ar_param=[0.9], sigma=0.5, start_value=[0.0])
             irreg_timeseries = ts.TimeSeries(irreg_signalgen, noise_generator=noise)
-            
-            #Generate regular
-            samples, signals, errors = reg_timeseries.sample(regular_time_samples)
-            signals-=0.0
-            signals[signals<0]=0
-            #Generate defect
-            defect_samples, defect_signals, defect_errors = irreg_timeseries.sample(irregular_time_samples)
-            defect_signals-=2.0
-            defect_signals[defect_signals<0]=0
-            #save as Dataframe
-            data = pd.DataFrame({'signal':signals+defect_signals,'labels':[1 if x== 0 else -1 for x in defect_signals] })
-            #Data column
-            self.columns = ['signal']
 
+            # Generate regular
+            samples, signals, errors = reg_timeseries.sample(regular_time_samples)
+            samples -= 0.0
+            samples[samples < 0] = 0
+
+            # Generate defect
+            defect_samples, defect_signals, defect_errors = irreg_timeseries.sample(irregular_time_samples)
+            defect_samples -= 2.0
+            defect_samples[defect_samples < 0] = 0
+
+            # save as Dataframe
+            data = pd.DataFrame(
+                {'samples': samples + defect_samples, 'labels': [1 if x == 0 else -1 for x in defect_samples]})
+
+        # Assume data column is always 'samples'
+        self.columns = ['samples']
         self.data = data
         self.process_data()
-        # print(signals)
-        #print(self.all_data[200,:,:], self.labels[200])
-
 
     def process_data(self):
         #Normalization
@@ -83,7 +82,7 @@ class TS_dataset(Dataset):
     def __len__(self):
         return len(self.labels)
 
-    def __getitem__(self,idx):
+    def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         data = self.all_data[idx,:,:]
