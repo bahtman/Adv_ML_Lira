d16c873ed0134b49f959b4c32215a11eb17d2839
Carl Johan Astrup <carlisafk@live.dk>
2021-05-04 18:07:05 +0200

    cleanup

--- vrae/detect.py
+++ vrae/detect.py
@@ -5,16 +5,8 @@ from sklearn.metrics import accuracy_score
 
 def detect(model, test_dataset, device):
     anomalies = model.detect_outlier(test_dataset, amount_of_samplings=10)    
-
-    cdict = { 1: 'red', 0: 'blue' }
     labels = test_dataset.labels
-    data = test_dataset.all_data
-
     accuracy = accuracy_score(labels, anomalies)
     print(accuracy)
     print(dict(zip(*np.unique(labels, return_counts=True))))
     print(dict(zip(*np.unique(anomalies, return_counts=True))))
-
-
-
-
