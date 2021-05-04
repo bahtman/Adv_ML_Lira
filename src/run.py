ceb9c74d7be886ef26f3d37bbbaa52ea37a1333f
Carl Johan Astrup <carlisafk@live.dk>
2021-05-04 18:06:31 +0200

    chcekout main

--- src/run.py
+++ src/run.py
@@ -3,28 +3,27 @@ import argparse
 import logging
 from dataset import TS_dataset
 from network import VAE
-#from NormalLSTM import RecurrentAutoencoderLSTM
+from NormalLSTM import RecurrentAutoencoderLSTM
+from TrainScript import train_model
 from torch.utils.data import DataLoader, random_split
-import torch.nn as nn
-from torch import load
+
 import os
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 PARSER = argparse.ArgumentParser(description='runModel.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
 # Actions
 PARSER.add_argument('--train', action='store_true', help='Train a new or restored model.')
 PARSER.add_argument('--generate', action='store_true', help='Generate samples from a model.')
-PARSER.add_argument('--detect', action='store_true', help='Detect outliers by using pre-trained model.')
 PARSER.add_argument('--cuda', type=int, help='Which cuda device to use')
 PARSER.add_argument('--seed', type=int, default=1, help='Random seed.')
 PARSER.add_argument('--model', type=int, default=1, help='Choose model for training if "1" the model will be a VAE, if "2" the model will be a normal LSTM.')
 
 # File paths
-PARSER.add_argument('--data_dir', default=f'{BASE_PATH}/Data/resampled_df.pickle', help='Location of dataset.')
-PARSER.add_argument('--output_dir', default=f'{BASE_PATH}/results/')
-PARSER.add_argument('--results_file', default=f'{BASE_PATH}/results.txt', help='Filename where to store settings and test results.')
+PARSER.add_argument('--data_dir', default='./Data/resampled_df.pickle', help='Location of dataset.')
+PARSER.add_argument('--output_dir', default='./results/')
+PARSER.add_argument('--results_file', default='results.txt', help='Filename where to store settings and test results.')
 PARSER.add_argument('--dataset', default='generated', help='Which dataset to use. [generated|GM]')
-PARSER.add_argument('--trained_model', default=f'{BASE_PATH}/boi.model', help='Path of pretrained model.')
 
 # Training parameters
 PARSER.add_argument('--batch_size', type=int, default=100)
@@ -37,18 +36,24 @@ PARSER.add_argument('--embedding_dim', type=int, default=64, help='Embedding dim
 PARSER.add_argument('--n_layers', type=int, default=12, help='Embedding dimension')
 PARSER.add_argument('--amount_of_plots', type = int, default = 6, help = 'The amount of inputs sequences and their respective reconstructions to be plotted')
 
+import torch.nn as nn
+
 
 
 if __name__ == '__main__':
     # Setup logging
     logging.basicConfig(
-        format="%(levelname) -10s %(asctime)s %(module)s:%(lineno)s %(message)s",
+        format="%(levelname) -10s %(asctime)s %(module)s:%(lineno)s %(funcName)s %(message)s",
         level=logging.INFO,
         datefmt='%Y-%m-%d %H:%M:%S')
 
     ARGS = PARSER.parse_args()
     # Logging parameters for debugging purposes
     logging.info(ARGS)
+
+    if ARGS.data_dir and not os.path.isfile(ARGS.data_dir):
+        logging.error(f'{ARGS.data_dir} doesn\'t exists')
+        exit(1)
 
     if not os.path.isdir(ARGS.output_dir):
         os.makedirs(ARGS.output_dir)
@@ -61,14 +66,12 @@ if __name__ == '__main__':
     else: 
         torch.manual_seed(ARGS.seed)
     if ARGS.dataset == 'generated':
-        logging.info('Using generated dataset')
         dataset = TS_dataset(timesteps=ARGS.time_steps)
         n_features = 1
         seq_len = ARGS.time_steps
     elif ARGS.dataset == 'GM':
-        logging.info('Using GreenMobility dataset')
         columns= ['acc.xyz.z']
-        dataset = TS_dataset(ARGS.data_dir,ARGS.time_steps, columns=columns)
+        dataset = TS_dataset(ARGS.data_dir,ARGS.time_steps,columns=columns)
         n_features= len(columns)
         seq_len = ARGS.time_steps
     else:
@@ -76,93 +79,39 @@ if __name__ == '__main__':
 
     if ARGS.model == 1:
         model = VAE(n_features, ARGS).to(ARGS.device)
-        logging.info("A VAE model type structure will be used for training")
+        print("A VAE model type structure will be used for training")
     elif ARGS.model == 2:
         model = RecurrentAutoencoderLSTM(seq_len, n_features, ARGS.embedding_dim, ARGS.latent_dim).to(ARGS.device)
-        logging.info("A normal LSTM model type structure will be used for training")
-    
-    # Set 10% validation
+        print("A normal LSTM model type structure will be used for training")
+ 
     val_percent = 0.1
     n_val = int(len(dataset) * val_percent)
     n_train = len(dataset) - n_val
     train, val = random_split(dataset, [n_train, n_val])
     
     # make datasets iterable
-    #n_val = int(len(val)*0.5)
-    #n_test = int(len(val)-n_val)
-    #val, test = random_split(val, [n_val, n_test])
-    train_loader = DataLoader(train, batch_size=ARGS.batch_size, shuffle=True, num_workers=0, drop_last=False)
-    val_loader = DataLoader(val, batch_size=ARGS.batch_size, shuffle=False, num_workers=0, drop_last=False)
-    
-    if ARGS.train:
-        from TrainScript import train_model
-        train_loader.dataset.isTrain = True
-        val_loader.dataset.isTrain = True
-        trained_model, history, train_diagnostics, val_diagnostics = train_model(
-            model,
-            train_loader,
-            val_loader,
-            ARGS
-        )
+    n_val = int(len(val)*0.5)
+    n_test = int(len(val)-n_val)
+    val, test = random_split(val, [n_val, n_test])
 
-    test_dataset = dataset
-    test_dataset.isTrain = False
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=0, drop_last=False)
+    train_loader = DataLoader(train, batch_size=ARGS.batch_size, shuffle=True, num_workers=0, drop_last=True)
+    val_loader = DataLoader(val, batch_size=ARGS.batch_size, shuffle=False, num_workers=0, drop_last=True)
+    test_loader = DataLoader(test, batch_size=1, shuffle=False, num_workers=0, drop_last=True)
+
+    if ARGS.train:
+        trained_model, history, train_diagnostics, val_diagnostics = train_model(
+        model,
+        train_loader,
+        val_loader,
+        ARGS
+        )
 
     if ARGS.generate:
         import matplotlib.pyplot as plt
         from ReconstructionPlotScript import Reconstruct_function as recfunct1
         from ReconstructionPlotScriptTest2 import Reconstruct_function as recfunct2
-        fig, axs = recfunct1(trained_model, test_loader, 5, ARGS)
+        #from plotting import make_vae_plots
+        fig, axs = recfunct1(trained_model, train_loader, 5, ARGS)
         fig, axs = recfunct2(trained_model, test_loader, train_diagnostics, val_diagnostics, ARGS)
+        #make_vae_plots(trained_model, x, y, outputs, training_data, validation_data)
         plt.show()
-
-    if ARGS.detect:
-        if not os.path.exists(ARGS.trained_model):
-            logging.error(f"Pre-trained model {ARGS.trained_model} not found. Use --trained_model with valid path")
-            exit(1)
-
-        from anomaly_detect_proto import detect
-        logging.info('Detecting outliers...')
-
-        args = DotMap(dict(
-            seq_len=369,
-            n_features = n_features,
-            batch_size = config.batch_size,
-            n_epochs = config.n_epochs,
-            optimizer = 'Adam',
-            clip = True,
-            loss = 'MSELoss',
-            block = 'LSTM',    
-            datafile = f'{os.path.dirname(os.path.abspath(__file__))}/data',
-            seed = 42,
-            results_file = 'result.txt',
-            output_dir = 'results',
-            visualize=True,
-            detectOutlier = True
-        ))
-        args.device = True if torch.cuda.is_available() else False
-        torch.manual_seed(args.seed)
-        #os.mkdir(args.output_dir)
-        if args.device == True : torch.cuda.manual_seed(args.seed)
-
-        vrae = VRAE(sequence_length=args.seq_len,
-                    number_of_features = args.n_features,
-                    hidden_size = config.hidden_size, 
-                    hidden_layer_depth = config.hidden_layer_depth,
-                    latent_length = config.latent_length,
-                    batch_size = args.batch_size,
-                    learning_rate = config.learning_rate,
-                    n_epochs = args.n_epochs,
-                    dropout_rate = config.dropout_rate,
-                    optimizer = args.optimizer, 
-                    cuda = args.device,
-                    clip=args.clip, 
-                    max_grad_norm=config.max_grad_norm,
-                    loss = args.loss,
-                    block = args.block,
-                    plot_loss = args.visualize)
-   
-        vrae.load('vrae/models/model.pth')
-        detect(vrae, test_loader, ARGS.device)
-
